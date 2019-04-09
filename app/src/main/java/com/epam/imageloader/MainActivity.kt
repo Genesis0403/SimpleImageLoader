178c50b36608bd1a090dc421c26c022f329f1ccc
Vlad Korotkevich <vlad.korotkevich2@gmail.com>
2019-04-09 20:51:24 +0300

    Save and restoration of image implementation.

--- app/src/main/java/com/epam/imageloader/MainActivity.kt
+++ app/src/main/java/com/epam/imageloader/MainActivity.kt
@@ -2,7 +2,10 @@ package com.epam.imageloader
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
@@ -15,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        println("CREATED")
         setContentView(R.layout.activity_main)
 
         val loadButton = findViewById<Button>(R.id.load_button)
@@ -23,6 +27,17 @@ class MainActivity : AppCompatActivity() {
             imageIntent.type = "image/*"
             startActivityForResult(imageIntent, PICK_IMAGE_REQUEST)
         }
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        main_image.setImageBitmap(savedInstanceState?.get("bitmap") as Bitmap)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        val drawable = main_image.drawable as? BitmapDrawable
+        outState?.putParcelable("bitmap", drawable?.bitmap)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
