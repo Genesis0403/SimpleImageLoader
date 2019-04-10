66dc0c042f6477d9569dbc8e9f706649ad151905
Vlad Korotkevich <vlad.korotkevich2@gmail.com>
2019-04-10 20:02:06 +0300

    MainActivity imageUri now has Uri.EMPTY. Docs added.

--- app/src/main/java/com/epam/imageloader/MainActivity.kt
+++ app/src/main/java/com/epam/imageloader/MainActivity.kt
@@ -9,46 +9,61 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Class which loads image to [mainImage] from gallery on [loadButton] press.
+ *
+ * @author Vlad Korotkevich
+ */
+
 class MainActivity : AppCompatActivity() {
+
+    private var imageUri = Uri.EMPTY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadImage()
+
+        loadButton.setOnClickListener {
+            loadImage()
+        }
     }
 
     private fun loadImage() {
-        loadButton.setOnClickListener {
-            val imageIntent = Intent(Intent.ACTION_PICK)
-            imageIntent.type = INTENT_IMAGE_TYPE
-            startActivityForResult(imageIntent, PICK_IMAGE_REQUEST)
-        }
+        val imageIntent = Intent(Intent.ACTION_PICK)
+        imageIntent.type = INTENT_IMAGE_TYPE
+        startActivityForResult(imageIntent, PICK_IMAGE_REQUEST)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
         super.onRestoreInstanceState(savedInstanceState)
-        val bitmap = MediaStore.Images.Media.getBitmap(
-            contentResolver,
-            imageUri
-        )
-        mainImage.setImageBitmap(bitmap)
+        imageUri = savedInstanceState?.get(IMAGE_URI) as? Uri
+        setImageBitmap()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(IMAGE_URI, imageUri)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
             imageUri = data?.data
-            val image = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
-            mainImage.setImageBitmap(image)
+            setImageBitmap()
         } else {
             Toast.makeText(this, getString(R.string.wrong_result), Toast.LENGTH_SHORT).show()
         }
     }
 
+    private fun setImageBitmap() {
+        if (imageUri == Uri.EMPTY || imageUri == null) return
+        val image = MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+        mainImage.setImageBitmap(image)
+    }
+
     private companion object {
         private const val PICK_IMAGE_REQUEST = 1
         private const val INTENT_IMAGE_TYPE = "image/*"
-        private var imageUri: Uri? = null
+        private const val IMAGE_URI = "imageUri"
     }
 }
